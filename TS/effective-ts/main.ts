e47d8e7461ec65bbf5388154b3731523a0b053c1
Hank <77097180+ranjafunc@users.noreply.github.com>
2022-07-21 22:58:48 +0900

    update til

--- TS/effective-ts/main.ts
+++ TS/effective-ts/main.ts
@@ -1,12 +1,59 @@
-function add(a: number, b: number): string;
-function add(a: number, b: string): number;
-function add(a: number, b: unknown): string | number {
-	if (typeof b === "number") {
-		return `${a + b}`;
+interface NamedVetor {
+	name: string;
+	x: number;
+	y: number;
+}
+
+interface Vetor2D {
+	x: number;
+	y: number;
+}
+function calcLength(v: Vetor2D) {
+	return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+const v = { x: 3, y: 4, name: "Zee" };
+const z = { x: 3, y: 4, name: "Zee", 1: "SPSPSPSP" };
+console.log(calcLength(v));
+console.log(calcLength(z));
+
+interface Vector3D {
+	x: number;
+	y: number;
+	z: number;
+}
+
+function normalize(v: Vector3D) {
+	// const length = calcLength(v);
+	const length = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+
+	return {
+		x: v.x / length,
+		y: v.y / length,
+		z: v.z / length, // 왜 이게 1이 나와야하는지 이해가 안되네;;
+	};
+}
+
+// console.log(normalize({ x: 3, y: 4, z: 5 }));
+
+function calcLength1(v: Vector3D) {
+	let length = 0;
+
+	for (const axis of Object.keys(v)) {
+		const coord = v[axis];
+		length += Math.abs(coord);
 	}
-	if (typeof b === "string") {
-		return Number(a + b);
+	return length;
+}
+
+console.log(calcLength1({ x: 3, y: 4, z: 5 }));
+class C {
+	foo: string;
+	constructor(foo: string) {
+		this.foo = foo;
 	}
 }
-console.log(add(1, 2));
-console.log(add(1, "2"));
+
+const c = new C("instance of C");
+
+const d: C = { foo: "object literal" };
