35b395882704f66da5d3db057373ff708971b5f7
Hank <77097180+ranjafunc@users.noreply.github.com>
2022-07-31 16:56:08 +0900

    update til - effective item 12

--- README.md
+++ README.md
@@ -20,6 +20,7 @@
   - [09 - 타입 단언보다는 타입 선언을 사용하기](https://github.com/ranjafunc/TIL/blob/main/TS/effective-ts/09_타입_단언보다는_타입_선언을_사용하기.md)
   - [10 - 객체 래퍼 타입 피하기](https://github.com/ranjafunc/TIL/blob/main/TS/effective-ts/10_객체_래퍼_타입_피하기.md)
   - [11 - 잉여 속성 체크의 한계 인지하기](https://github.com/ranjafunc/TIL/blob/main/TS/effective-ts/11_잉여_속성_체크의_한계_인지하기.md)
+  - [12 - 함수 표현식에 타입 적용하기](https://github.com/ranjafunc/TIL/blob/main/TS/effective-ts/12_함수_표현식에_타입_적용하기.md)
 
 ### 3. `React`
 
--- TS/effective-ts/main.ts
+++ TS/effective-ts/main.ts
@@ -1,12 +1,17 @@
-interface Options {
-	title: string;
-	darkMode?: boolean;
-}
+type BinaryFn = (a: number, b: number) => number;
 
-const o1: Options = document;
-const o2: Options = new HTMLAnchorElement();
-const o: Options = { darkmode: false, title: "ski" };
-/*
-'{ darkmode: boolean; title: string; }' 형식은 'Options' 형식에 할당할 수 없습니다.
-  개체 리터럴은 알려진 속성만 지정할 수 있지만 'Options' 형식에 'darkmode'이(가) 없습니다. 'darkMode'을(를) 쓰려고 했습니까?
-*/
+const add: BinaryFn = (a, b) => a + b;
+const sub: BinaryFn = (a, b) => a - b;
+
+declare function fetch(
+	input: RequestInfo,
+	init: RequestInit
+): Promise<Response>;
+
+const checkedFetch: typeof fetch = async (input, init) => {
+	const response = await fetch(input, init);
+	if (!response.ok) {
+		throw new Error("Request failed: " + response.status);
+	}
+	return response;
+};
