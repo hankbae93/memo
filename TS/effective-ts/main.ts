439a2bb97158f18d32b002e07665c79628709c4f
Hank <77097180+ranjafunc@users.noreply.github.com>
2022-09-26 22:14:35 +0900

    Update: item 20 til

--- TS/effective-ts/main.ts
+++ TS/effective-ts/main.ts
@@ -1,20 +1,3 @@
-const cache: { [ticker: string]: number } = {};
+let id: string | number = "12-34-56";
 
-function getQuote(ticker: string): Promise<number> {
-	if (ticker in cache) {
-		return cache[ticker]; // ~~ 'number' 형식은 'Promise<number>' 형식에 할당할 수 없습니다.ts(2322)
-	}
-	return fetch(`https://quotes.example.com/?q=${ticker}`)
-		.then((response) => response.json())
-		.then((quote) => {
-			cache[ticker] = quote;
-			return quote;
-		});
-}
-
-getQuote("MSFT").then(() => {});
-/**
- any
-'number | Promise<any>' 형식에 'then' 속성이 없습니다.
-  'number' 형식에 'then' 속성이 없습니다.ts(2339)
- */
+id = 12;
